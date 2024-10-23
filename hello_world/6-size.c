ded5d0dbbe1ebdb780104dc14a4e161241a44b4f
Camiii7 <camilairina7@gmail.com>
2024-10-23 17:00:00 +0000

    updated

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - Function that prints the size depending on the type of
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	printf("Size of a char: %zu byte(s)\n", sizeof(char));
-    	printf("Size of an int: %zu byte(s)\n", sizeof(int));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-       	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int))
-	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int))
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 
-	 return (0);
+	return (0);
 }
