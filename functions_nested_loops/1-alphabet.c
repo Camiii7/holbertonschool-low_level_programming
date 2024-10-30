76d4c2a7628c66bb8579a43a8264323adb3b4243
Camiii7 <camilairina7@gmail.com>
2024-10-30 03:29:00 +0000

    Alfabeto en minuscula con _putchar

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Imprime el alfabeto en minuscula
+ * Return: void
+ */
+	void print_alphabet(void)
+	{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+
+	_putchar(c);
+	}
+	_putchar('\n');
+}
---
+++ functions_nested_loops/1-main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * main - Archivo de prueba
+ * Return: Always 0.
+ */
+	int main(void) {
+	print_alphabet();
+	return (0);
+}
