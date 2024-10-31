04ef48bbe0c4ee972aa81ff178448431bf63d494
Camiii7 <camilairina.f@gmail.com>
2024-10-31 19:22:37 +0000

    Funcion que imprima el ultimo digito de un numero

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,23 +1,21 @@
 #include "main.h"
 
 /**
- * print_last_digit - Imprime el ultimo di­gito de un uºmero.
- * @n: El numero del que obtener el uºltimo i­gito.
- *
+ * print_last_digit - Imprime el ultimo di­gito de u numero
+ * @n: El numero del que obtener el uºltimodigito
  * Return: Valor del ultimo digito.
  */
-int print_last_digit(int n)
-{
-    int last_digit;
+	int print_last_digit(int n)
+	{
+	int last_digit;
 
-    last_digit = n % 10;
+	last_digit = n % 10;
 
-    if (last_digit < 0)
-    {
-        last_digit = -last_digit;
-    }
+	if (last_digit < 0)
+	{
+	last_digit = -last_digit;
+	}
 
-    _putchar(last_digit + '0');
-    return (last_digit); 
-}
-
+	_putchar(last_digit + '0');
+	return (last_digit);
+	}
