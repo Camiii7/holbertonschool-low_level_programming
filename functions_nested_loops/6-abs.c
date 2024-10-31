31df75686a376ffcaabaa857f33052bdecc3ace4
Camiii7 <camilairina.f@gmail.com>
2024-10-31 17:52:43 +0000

    Funcion que calcule el valor absoluto de un entero

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - Calcula el valor absoluto de un entero.
+ * @n: El entero a verificar.
+ * Return: El valor absoluto del entero.
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+		return (n);
+}
