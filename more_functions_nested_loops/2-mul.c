b38ec521cae7359770d6491c639d368fcf7e6221
Camiii7 <camilairina7@gmail.com>
2024-11-02 16:47:02 +0000

    Multiplica dos enteros

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - Multiplica dos enteros.
+ * @a: Primer entero
+ * @b: Segundo entero
+ * Return: Producto de a * b.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,5 +1,5 @@
 #include <stdio.h>
 
-
+int mul(int a, int b);
 int _isdigit(int c);
 int _isupper(int c);
