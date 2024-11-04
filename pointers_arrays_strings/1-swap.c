3de2d89e647f693995cb8c057077184e76e88332
Camiii7 <camilairina7@gmail.com>
2024-11-04 14:24:41 +0000

    Funcion que intercambia los valoresde dos enteros

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - Intercambia los valores de dos enteros.
+ * @a: Puntero al primer entero.
+ * @b: Puntero al segundo entero.
+ * Return: 0
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
