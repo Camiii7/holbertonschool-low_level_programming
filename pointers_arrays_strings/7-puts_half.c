8a0c430c607635453af4770e4ad08bbacdad3653
Camiii7 <camilairina7@gmail.com>
2024-11-04 16:53:52 +0000

    Funcion que imprima la mitad de una cadena

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts_half - imprime la mitad de una cadena
+ * @str: la cadena a imprimir
+ */
+void puts_half(char *str)
+{
+	int length = 0, start, i;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	if (length % 2 == 0)
+	{
+		start = length / 2;
+	}
+	else
+	{
+		start = (length - 1) / 2 + 1;
+	}
+
+	for (i = start; i < length; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
