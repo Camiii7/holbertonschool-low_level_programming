dde599051059497cc1a181dfff35ab957098267b
Camiii7 <camilairina7@gmail.com>
2024-11-08 16:06:00 +0000

    arreglo 3

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -5,17 +5,16 @@
  * @dest: Puntero al area de memoria de destino
  * @src: Puntero al area de memoria de origen
  * @n: Numero de bytes a copiar
- *
  * Return: Puntero al area de memoria de destino (dest)
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    unsigned int i;
+	unsigned int i;
 
-    for (i = 0; i < n; i++)
-    {
-        dest[i] = src[i];
-    }
+	for (i = 0; i < n; i++)
+	{
+	dest[i] = src[i];
+	}
 
-    return (dest);
+	return (dest);
 }
