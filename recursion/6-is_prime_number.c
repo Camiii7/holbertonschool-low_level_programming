f04318061f4b1a347eec4f7f6d250591dd04867c
Camiii7 <camilairina7@gmail.com>
2024-11-11 21:57:22 +0000

    Agregue funcion auxiliar

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,30 +1,37 @@
 #include "main.h"
 
 /**
- * int is_prime_number - Funcion que devuelve 1 si es primo sino 0
- * @n: Num a verificar
- *
- * Return: 1 Si es primo sino 0
+ * is_prime_number - Funcion que devuelve 1 si es primo, sino 0
+ * @n: Numero a verificar
+ * Return: 1 si es primo, de lo contrario 0
  */
 
-
+int is_prime_helper(int n, int i);
 int is_prime_number(int n)
 {
-	int i;
-
 	if (n <= 1)
 	{
 		return (0);
 	}
+	return (is_prime_helper(n, 2));
+}
 
-	for (i = 2; i * i <= n; i++)
+/**
+ * is_prime_helper - Funcion auxiliar para verificar si un num es primo
+ * @n: Num a verificar
+ * @i: Posible divisor
+ * Return: 1 si es primo, sino 0
+ */
 
+int is_prime_helper(int n, int i)
+{
+	if (i * i > n)
 	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
-	}
-
 		return (1);
+	}
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	return (is_prime_helper(n, i + 1));
 }
