69e9a2e1a3459784e324733838722308c880053d
Camiii7 <camilairina7@gmail.com>
2024-11-11 01:33:41 +0000

    Funcion que imprime la longitud de la cadena

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * int _strlen_recursion - Funcion que devuelve la longitud de una cadena
+ * @s: La cadena
+ * Return: 0
+ */
+
+int _strlen_recursion(char *s)
+{
+
+	if (*s == '\0')
+	{
+		return(0);
+	}
+	
+	return 1 + _strlen_recursion(s + 1);
+}
