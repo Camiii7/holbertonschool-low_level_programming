d126f692c6d64df6878d82400d8d40bb19a39b1b
Camiii7 <camilairina.f@gmail.com>
2024-11-14 01:44:16 +0000

    Funcion que devuelve un puntero a una matriz bidimensional

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+/**
+ * alloc_grid - Crea una matriz de enteros e inicializa todos sus valores a 0
+ * @width: Num de columnas de la matriz.
+ * @height: Num de filas de la matriz.
+ * Return: Un puntero a la matriz creada o NULL si hay error.
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = malloc(sizeof(int *) * height);
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(grid[j]);
+			free(grid);
+			return (NULL);
+		}
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+
+	return (grid);
+}
