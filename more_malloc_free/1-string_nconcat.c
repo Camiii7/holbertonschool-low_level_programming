b9003d2a8b2e1fdb8bc117cb273a4e188ae6fe20
Camiii7 <camilairina7@gmail.com>
2024-11-16 00:51:47 +0000

    Funcion que concatena 2 cadenas, arreglo 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,39 +1,39 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
+
 
 /**
- * string_nconcat - Concatena 2 cadenas de caracteres.
- * @s1: primera cadena
- * @s2: segunda cadena
- * @n: Num de caracteres de s2 a concatenar
- * Return: puntero a la nueva cadena concatenada o NULL si hay error
+ * string_nconcat - Concatena 2 cadenas de caracteres
+ * @s1: Primer cadena
+ * @s2: Segunda cadena
+ * @n: Num maximo de caracteres a copiar de s2
+ * Return: Puntero a la cadena resultante
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *result;
-	unsigned int len1, len2, i, j;
+	unsigned int len1 = 0, len2 = 0, i, j;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	len1 = strlen(s1);
-	len2 = strlen(s2);
+	while (s1[len1] != '\0')
+		len1++;
 
-	if (n >= len2)
-		n = len2;
+	while (s2[len2] != '\0' && len2 < n)
+		len2++;
 
-	result = malloc(sizeof(char) * (len1 + n + 1));
-
+	result = malloc(len1 + len2 + 1);
 	if (result == NULL)
 		return (NULL);
 
 	for (i = 0; i < len1; i++)
 		result[i] = s1[i];
 
-	for (j = 0; j < n; j++)
+	for (j = 0; j < len2; j++)
 		result[i + j] = s2[j];
 
 	result[i + j] = '\0';
