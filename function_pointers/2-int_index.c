3e436152e1286d2ea9d60e93ec48afc442316945
Camiii7 <camilairina7@gmail.com>
2024-11-19 17:06:40 +0000

    Funcion que busca un enetero en un array

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - Busca el indice del elemento que cumple con la condicion
+ * @array: Arreglo de enteros
+ * @size: Taman√o del areglo
+ * @cmp: Puntero a la funcion de comparacion
+ * Return: El indice o -1 si no se encuentra
+ **/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 1)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+
+	return (-1);
+}
