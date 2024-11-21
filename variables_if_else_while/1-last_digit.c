b726622a2cbddfd3a6a4cf58a96bcd7e917705ed
Camiii7 <camilairina7@gmail.com>
2024-11-21 03:21:49 +0000

    Asigna num aleatorio a la variable n

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -4,31 +4,33 @@
 
 /**
  * main - Punto de entrada
- * Description: Genera numero aleatorio y evalua el ultimo digito
- * Return: Always 0
+ * n: Genera numero aleatorio y evalua el ultimo digito
+ * Return: 0
  **/
+
 int main(void)
 {
 	int n;
 	int last_digit;
 
-	srand(time(0)); /* Incializa para la aleatoriedad */
-	n = rand() - RAND_MAX / 2; /* Genera numero aleatorio */
-	last_digit = n % 10; /* Obtiene ultimo digito */
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last_digit = n % 10;
 
-	/* Imprime el resultado obtenido del ultimo digito */
+
 	if (last_digit > 5)
 	{
-	printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 	}
+	
 	else if (last_digit == 0)
 	{
-	printf("Last digit of %d is %d and is 0\n", n, last_digit);
+		printf("Last digit of %d is %d and is 0\n", n, last_digit);
 	}
 	else
-{
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
-}
+	{	
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+	}
 
-	return (0);
+		return (0);
 }
