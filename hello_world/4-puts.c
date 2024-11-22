a77e9b54b5d6001727805c8b41fa16e0c410c5e3
Camiii7 <camilairina7@gmail.com>
2024-11-22 20:47:00 +0000

    Arreglo de sintaxis

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Description: A program that prints the string
- * "Programming is like building a multilingual puzzle" using the puts function
- *
- * Return: Always 0 (Success)
+ * main - Punto de entrada
+ * Descripcion: Programa que imprime una cadena
+ * "Programming is like building a multilingual puzzle"
+ * Return: 0
  */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
