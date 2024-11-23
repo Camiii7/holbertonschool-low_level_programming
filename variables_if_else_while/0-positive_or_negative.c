69dd5a7bf4017113aaa047ae25ad827384f63f02
Camiii7 <camilairina7@gmail.com>
2024-11-23 15:41:59 +0000

    Arreglo de sintaxis

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -4,8 +4,9 @@
 
 /**
  * main - Determines if a number is positive, negative or zero
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
+
 int main(void)
 {
 	int n;
@@ -13,18 +14,18 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	/* Verifica si n es positivo, negativo o cero */
+	
 	if (n > 0)
 	{
-		printf("%d is positive\n", n);
+		printf("%d es positivo\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero\n", n);
+		printf("%d es cero\n", n);
 	}
 	else
 	{
-		printf("%d is negative\n", n);
+		printf("%d es negativo\n", n);
 	}
 
 	return (0);
