e8434a8fee07661ed221fc5ff6066d55d81edabf
Camiii7 <camilairina7@gmail.com>
2024-11-23 15:50:25 +0000

    Arreglo de sintaxis

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - Prints all the numbers of base 16 in lowercase.
- *
- * Return: Always 0 (Success)
+ * main - Imprime todos los numeros en base 16
+ * Retorna: 0 
  */
+
 int main(void)
 {
 	int num;
 
-	/* Imprime los nuºmeros del 0 al 9 */
 	for (num = 0; num < 10; num++)
 	{
 	putchar(num + '0');
 	}
 
-	/* Imprime las letras de 'a' a f */
 	for (num = 'a'; num <= 'f'; num++)
 	{
 	putchar(num);
