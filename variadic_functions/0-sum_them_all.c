e79e12a675e2764aa75f515fba08df35d778e5c9
Camiii7 <camilairina7@gmail.com>
2024-11-24 00:35:56 +0000

    Funcion que recibe un num variable de parametros y devuelve la suma de ellos

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - Suma todos los parametros proporcionados
+ * @n: Numero de parametros a sumar
+ * Return: la suma de todos los parámetros, o 0 si n es 
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	int sum = 0;
+	unsigned int i;
+
+	if (n == 0)
+		return (0);
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+
+	va_end(args);
+	return (sum);
+}
+
