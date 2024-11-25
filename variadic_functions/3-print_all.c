13b4b16d94cca15b15d2d8c1d0cf14d83f674046
Camiii7 <camilairina7@gmail.com>
2024-11-25 19:58:10 +0000

    Funcion que imprime B, 3, stSchool

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,49 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_all - Imprime cualquier cosa segun formato dado
+ * @format: Cadena que especifica los tipos de argumentos
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int i = 0;
+	char *str;
+	char *sep = "";
+
+	va_start(args, format);
+	while (format && format[i])
+	{
+		if (format[i] == 'c' || format[i] == 'i' ||
+		    format[i] == 'f' || format[i] == 's')
+		{
+			printf("%s", sep);
+			switch (format[i])
+			{
+				case 'c':
+					printf("%c", va_arg(args, int));
+					break;
+				case 'i':
+					printf("%d", va_arg(args, int));
+					break;
+				case 'f':
+					printf("%f", va_arg(args, double));
+					break;
+				case 's':
+					str = va_arg(args, char *);
+					if (str == NULL)
+						printf("(nil)");
+					else
+						printf("%s", str);
+					break;
+			}
+			sep = ", ";
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
+
