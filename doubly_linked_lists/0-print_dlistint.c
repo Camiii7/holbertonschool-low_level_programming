8a29020d62060e72d67cd92453b09ab9c1c9641f
Camiii7 <camilairina.f@gmail.com>
2024-12-12 18:29:58 +0000

    Imprime todos los elementos de la lista

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - Imprime los elementos de la lista doblemente enlazada
+ * @h: Puntero al inicio de la lista
+ * Return: Cantidad de nodos impresos
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL) /* Se ejecuta mientras h no sea NULL */
+    {
+	printf("%d\n", h->n);  /* Imprime el valor del nodo */
+	count++;               /* Incrementa el contador  */
+	h = h->next;            /* Avanza al siguiente nodo */
+    }
+
+	return count;  /* Devuelve el número de nodo*/
+}
